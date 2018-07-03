d4ba5f97edfe7ab4db03abe392892c5abb355fbe
Chris Cesare <chris.cesare@gmail.com>
2018-07-03 08:14:02 -0400

    Added webpack infrastructure

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // webpack folder's entry js - excluded from jekll's build process.
+    mode: "development",
+    entry: "./webpack/entry.js",
+    output: {
+        // we're going to put the generated file in the assets folder so jekyll will grab it.
+        path: __dirname + "/assets/js/",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader", // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ["react", "es2015"]
+                }
+            }
+        ]
+    }
+};
